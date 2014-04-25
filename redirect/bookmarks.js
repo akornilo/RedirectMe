1e6bd312e046b91e2883587847acc40b60ae0e7d
Nastia <annie237@gmail.com>
2014-04-25 16:54:59 -0400

    move bookmarks yay

--- redirect/bookmarks.js
+++ redirect/bookmarks.js
@@ -17,7 +17,33 @@ function process_bookmark(bookmarks) {
 chrome.bookmarks.search("RedirectMeUnread", process_bookmark)
 
 
+//given a url, finds the chrome bookmark and moves it into another folder
+//use search to find the page in the bookmarks
+//use search to find the ReadirectMeRead folder 
+//then use move to move it into the ReadirectMeRead
+
+function moveUrl(pageURL) {
+	//Find read folder
+  console.log(":(")
+	chrome.bookmarks.search("RedirectMeRead", function(read){
+			console.log(read);
+		if (read.length > 0){
+			var folder = read[0];
+			//Search for the bookmarks that match the URL clicked and move them into the read folder 
+      chrome.bookmarks.search(pageURL, function(results){
+        for (var i = 0; i < results.length; i++){
+          
+				  console.log(results[i], folder);
+					chrome.bookmarks.move(results[i].id, {parentId: folder.id}, function(result) { console.log("STRING");});
+        }
+			})
+		}
+	})	
+}
+
+
 $(document).on('click', '#url', function () {
+  moveUrl(this.href);
   console.log(this.href);
 });
 
