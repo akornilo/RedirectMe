ce0f27a86783b379b8d5dffe77f260411812cd79
Nastia <annie237@gmail.com>
2014-04-25 15:42:30 -0400

    sorta broken but works

--- redirect/bookmarks.js
+++ redirect/bookmarks.js
@@ -1,20 +1,27 @@
+//Find bookmarks to print out
 function process_bookmark(bookmarks) {
 
   bId = bookmarks[0].id;
   
   chrome.bookmarks.getChildren(bId, function(child){
     var s = "";
-    console.log(child);
     for (var i = 0; i < child.length; i++){
       if (child[i].url) {
-        s+=("<p class = \"link\"><a href = "+child[i].url+">"+ child[i].title + "</a></p>");
+        $("#content").append("<p class = \"link\"><a href = "+child[i].url+">"+ child[i].title + "</a></p>");
       }
     }
-    $("#content").html(s);
   
   });
 }
 chrome.bookmarks.search("RedirectMeUnread", process_bookmark)
 
+//Move bookmarks to read when clicked
+//Neither of these attempts works properly :(
+$( "#url" ).click(function() {
+    console.log( "Handler for .click() called." );
+});
 
+$(document).on('click', 'p', function () {
+      console.log(this);
+});
 
